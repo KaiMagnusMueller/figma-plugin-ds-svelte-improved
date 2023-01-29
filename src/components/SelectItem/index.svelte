5fec6cd2042d4823d4cfb317882e468d5d047d91
KaiMagnusMueller <ndsignin@outlook.de>
2023-01-30 00:20:51 +0100

    Add hidden attribute to select item
    - Update formatting rules

--- src/components/SelectItem/index.svelte
+++ src/components/SelectItem/index.svelte
@@ -1,70 +1,85 @@
 <script>
-    export let itemId;
-    export let selected = false;
-    export { className as class };
-    export let rounded = false;
-
-    let className = '';
+	export let itemId;
+	export let selected = false;
+	export { className as class };
+	export let rounded = false;
+	export let disabled = false;
+	let className = '';
 </script>
 
-<li {itemId} tabindex={itemId + 1} class:highlight={selected} class={className} class:rounded on:mouseenter on:click>
-    <div class="icon" class:selected />
-    <div class="label"><slot /></div>
+<li
+	{itemId}
+	tabindex={itemId + 1}
+	class:highlight={selected}
+	class={className}
+	class:rounded
+	class:disabled
+	on:mouseenter
+	on:click
+>
+	<div class="icon" class:selected />
+	<div class="label"><slot /></div>
 </li>
 
 <style>
-    li {
-        align-items: center;
-        color: var(--color-text-menu-text);
-        cursor: default;
-        display: flex;
-        font-family: var(--font-stack);
-        font-size: var(--font-size-small);
-        font-weight: var(--font-weight-normal);
-        letter-spacing: var(--font-letter-spacing-neg-xsmall);
-        line-height: var(--font-line-height);
-        height: var(--size-small);
-        padding: 0px var(--size-xsmall) 0px var(--size-xxsmall);
-        user-select: none;
-        outline: none;
-        transition-property: background-color;
-        transition-duration: 30ms;
-    }
+	li {
+		align-items: center;
+		color: var(--color-text-menu-text);
+		cursor: default;
+		display: flex;
+		font-family: var(--font-stack);
+		font-size: var(--font-size-small);
+		font-weight: var(--font-weight-normal);
+		letter-spacing: var(--font-letter-spacing-neg-xsmall);
+		line-height: var(--font-line-height);
+		height: var(--size-small);
+		padding: 0px var(--size-xsmall) 0px var(--size-xxsmall);
+		user-select: none;
+		outline: none;
+		transition-property: background-color;
+		transition-duration: 30ms;
+	}
 
-    .label {
-        overflow-x: hidden;
-        white-space: nowrap;
-        text-overflow: ellipsis;
-        pointer-events: none;
-    }
+	li.disabled {
+		pointer-events: none;
+		color: var(--figma-color-text-disabled);
+		display: none;
+	}
 
-    .highlight,
-    li:hover,
-    li:focus {
-        background-color: var(--figma-color-bg-brand);
-    }
+	.label {
+		overflow-x: hidden;
+		white-space: nowrap;
+		text-overflow: ellipsis;
+		pointer-events: none;
+	}
 
-    .icon {
-        width: var(--size-xsmall);
-        height: var(--size-xsmall);
-        margin-right: var(--size-xxsmall);
-        opacity: 0;
-        pointer-events: none;
-        background-image: url('data:image/svg+xml;utf8,%3Csvg%20fill%3D%22none%22%20height%3D%2216%22%20viewBox%3D%220%200%2016%2016%22%20width%3D%2216%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%3Cpath%20clip-rule%3D%22evenodd%22%20d%3D%22m13.2069%205.20724-5.50002%205.49996-.70711.7072-.70711-.7072-3-2.99996%201.41422-1.41421%202.29289%202.29289%204.79293-4.79289z%22%20fill%3D%22%23fff%22%20fill-rule%3D%22evenodd%22%2F%3E%3C%2Fsvg%3E');
-        background-repeat: no-repeat;
-        background-position: center center;
-    }
-    .icon.selected {
-        opacity: 1;
-    }
+	.highlight,
+	li:hover,
+	li:focus {
+		background-color: var(--figma-color-bg-brand);
+	}
 
-    .blink,
-    .blink:hover {
-        background-color: transparent;
-    }
+	.icon {
+		width: var(--size-xsmall);
+		height: var(--size-xsmall);
+		margin-right: var(--size-xxsmall);
+		opacity: 0;
+		pointer-events: none;
+		background-image: url('data:image/svg+xml;utf8,%3Csvg%20fill%3D%22none%22%20height%3D%2216%22%20viewBox%3D%220%200%2016%2016%22%20width%3D%2216%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%3Cpath%20clip-rule%3D%22evenodd%22%20d%3D%22m13.2069%205.20724-5.50002%205.49996-.70711.7072-.70711-.7072-3-2.99996%201.41422-1.41421%202.29289%202.29289%204.79293-4.79289z%22%20fill%3D%22%23fff%22%20fill-rule%3D%22evenodd%22%2F%3E%3C%2Fsvg%3E');
+		background-repeat: no-repeat;
+		background-position: center center;
+	}
+	.icon.selected {
+		opacity: 1;
+	}
 
-    .rounded {
-        padding: 0 var(--size-xsmall) 0 var(--size-xxxsmall);
-        border-radius: 3px;
-    }
+	.blink,
+	.blink:hover {
+		background-color: transparent;
+	}
+
+	.rounded {
+		padding: 0 var(--size-xsmall) 0 var(--size-xxxsmall);
+		border-radius: 3px;
+	}
 </style>
